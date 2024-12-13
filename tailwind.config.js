fa905f06e1c68dc4b911951247a707a91938bbb7
t-sweta <swetashreya004@gmail.com>
2024-12-14 01:55:02 +0530

    Initial project load

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#202124",
+        secondary: "#2B3136",
+        primHead: "#FFF7B0",
+        white: "#EFEFEF",
+      },
+    },
+  },
+  plugins: [],
+};
